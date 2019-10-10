39b6270364e9f64352c5d7c117478bc5ca2d93ed
Guillermo Vera <vfguille@gmail.com>
2019-10-10 11:14:16 +0200

    Login and splash

--- app/src/main/java/com/vfguille/inventory/LoginActivity.java
+++ app/src/main/java/com/vfguille/inventory/LoginActivity.java
@@ -1,13 +1,14 @@
 package com.vfguille.inventory;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
