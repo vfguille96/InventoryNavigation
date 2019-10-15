334583ea85dbb72a6b87066ad81104ed1bb1a354
Guillermo Vera <vfguille@gmail.com>
2019-10-15 12:44:17 +0200

    Login y splash delay

--- app/src/main/java/com/vfguille/inventory/SplashActivity.java
+++ app/src/main/java/com/vfguille/inventory/SplashActivity.java
@@ -1,13 +1,44 @@
 package com.vfguille.inventory;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final long WAIT_TIME = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    /*
+        Se ejecuta el método en un hilo diferente al de la UI.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        /*Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                    initLogin();
+            }
+        };*/
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initLogin();
+            }
+        }, WAIT_TIME);
+    }
+
+    private void initLogin() {
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
