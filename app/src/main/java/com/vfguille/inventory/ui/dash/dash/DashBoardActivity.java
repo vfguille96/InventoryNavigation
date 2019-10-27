6056c1be2763f20debc35b93bdd9565cff8ffdc5
Guillermo Vera <vfguille@gmail.com>
2019-10-27 17:46:10 +0100

    Dash

--- app/src/main/java/com/vfguille/inventory/ui/dash/dash/DashBoardActivity.java
+++ app/src/main/java/com/vfguille/inventory/ui/dash/dash/DashBoardActivity.java
@@ -1,18 +1,45 @@
 package com.vfguille.inventory.ui.dash.dash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.vfguille.inventory.R;
 
 public class DashBoardActivity extends AppCompatActivity {
+    FloatingActionButton floatingActionButton;
+    BottomAppBar bottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash_board);
+        setContentView(R.layout.activity_dash_board_material);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        bottomAppBar =  findViewById(R.id.bottomAppBar);
+        setSupportActionBar(bottomAppBar);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.profileMenu:
+                return true;
+            case R.id.settingsMenu:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
